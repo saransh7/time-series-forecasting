d5e580cfc8c6a8047122b2a32f30ab3db2e52822
saransh7 <sar.bhat0772@yahoo.com>
2020-02-13 20:14:42 +0530

    wip: exponential smoothing

--- models/exponential_smoothing.py
+++ models/exponential_smoothing.py
@@ -1,7 +1,9 @@
 import numpy as np
+import model_params as params
 from utils.split_util import *
 from statsmodels.tsa.holtwinters import ExponentialSmoothing
 from statsmodels.tsa.api import SimpleExpSmoothing
+from sklearn.metrics import mean_squared_error
 
 def ets(demand, validation_points, trend, seasonal):
     model_count = 0
@@ -13,8 +15,8 @@ def ets(demand, validation_points, trend, seasonal):
                 try:
                     ets_fit = ExponentialSmoothing(demand_train, trend=param_trend,
                                                    seasonal=param_seasonal, seasonal_periods=c.seasons).fit()
-                    ets_fcast = ets_fit.forecast(steps=c.valid_months)
-                    error = f.root_mean_square_error(demand_valid, ets_fcast)
+                    ets_fcast = ets_fit.forecast(steps=3)
+                    error = mean_squared_error(demand_valid, ets_fcast)
                 except:
                     error = float('Inf')
                 ets_cv_error = np.append(ets_cv_error, error)
@@ -29,10 +31,10 @@ def ets(demand, validation_points, trend, seasonal):
                 ets_best_model = [param_trend, param_seasonal]
             model_count = model_count + 1
 
-    ets_best_fit = ExponentialSmoothing(demand_train_valid,
+    ets_best_fit = ExponentialSmoothing(demand,
                                         trend=ets_best_model[0],
                                         seasonal=(None if ets_best_model[1] is None else ets_best_model[1]),
-                                        seasonal_periods=c.seasons,
+                                        seasonal_periods=params.seasons,
                                         damped=True
                                         ).fit()
     return [ets_best_model, ets_best_error, ets_best_fit]
